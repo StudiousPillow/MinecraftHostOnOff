014118cc49baa5a2b60690ecc896ed8ba8abdb10
StudiousPillow <151650879+StudiousPillow@users.noreply.github.com>
2025-02-28 17:09:52 +0100

    communication scripts

---
+++ client_command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source params.sh
+
+# Check if a command is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <command_to_give_minecraft_server>"
+    exit 1
+fi
+
+# Command to send
+COMMAND="$1"
+
+# Send the command to the remote server
+echo "Sending command: $COMMAND"
+ssh $USER@$SERVER_ADDRESS "echo $COMMAND | nc localhost $SERVER_PORT"
--- server_listener.sh
+++ server_listener.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
+source params.sh ## to import the PORT and SCREEN_NAME
 
-# Port to listen on
-# PORT=12345
-source params.sh
-
-# Command to execute when the signal is received
-COMMAND="./server_stop.sh"
-
-# Listen on the specified port
+# Listen for incoming SSH connections
 while true; do
-    echo "Listening on port $PORT..."
-    nc -l -p $PORT | while read line; do
-        if [[ "$line" == "$STOP_MSG" ]]; then
-            echo "Received stop signal. Executing command..."
-            $COMMAND
-            break 2  # Exit both the inner and outer loops to stop the script
-        elif [[ "$line" == "$INJECT_MSG" ]]; then
-            echo "Received inject signal. Injecting command into Minecraft server..."
-            screen -S $SCREEN_NAME -X stuff "$INJECT_COMMAND$(printf \\r)"
-        # screen -S $SCREEN_NAME -X stuff "stop$(printf \\r)"
+    echo "Listening on port $LISTEN_PORT..."
+    nc -l -p $LISTEN_PORT | while read line; do
+        # Execute the received command
+        screen -S $SCREEN_NAME -X stuff "$line$(printf \\r)"
     done
 done
