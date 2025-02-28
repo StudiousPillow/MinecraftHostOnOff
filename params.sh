5a15f593cea1c8f2150c34b87106940a30bbf6a9
StudiousPillow <151650879+StudiousPillow@users.noreply.github.com>
2025-02-28 19:18:57 +0100

    params.sh

---
+++ params.sh
@@ -0,0 +1,11 @@
+#client
+SERVER_PORT="25565" ## to connect to server
+SERVER_ADDRESS="localhost" ## IP of the server (to connect and to send commands)
+USER="User1" ## to send commands through ssh
+
+#server
+JAR_NAME='forge-1.21.4-54.1.0-shim.jar' # to launch the server
+SCREEN_NAME="minecraft_server"
+
+#both
+LISTEN_PORT="56552" ## to send commands
