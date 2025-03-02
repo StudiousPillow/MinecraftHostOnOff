6192adee17150136b9930bf80a7a2a61fd37e2da
StudiousPillow <151650879+StudiousPillow@users.noreply.github.com>
2025-03-02 21:13:01 +0100

    added server_listener

--- server_start.sh
+++ server_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # java -Xmx1024M -Xms1024M -jar forge-1.21.4-54.1.0-shim.jar
 
-# SCREEN_NAME="minecraft_server" # needs to be the same as in server_stop.sh
-# JAR_NAME='forge-1.21.4-54.1.0-shim.jar' # replace with the right filename
 source params.sh
 
 ## start the server in a screen session to easily stop it
 screen -S $SCREEN_NAME -d -m java -Xmx1024M -Xms1024M \
  -jar $JAR_NAME nogui 
+
+screen -S server_listener -d -m bash server_listener.sh
