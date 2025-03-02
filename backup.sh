16155bb042fdf2134a8c486a81ebc07647a9d807
StudiousPillow <151650879+StudiousPillow@users.noreply.github.com>
2025-03-03 00:36:57 +0100

    fix backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 source params.sh
 mkdir server_backup
-rsync -arvz --delete -e "ssh -i $SSHKEY" $USER@$SERVER_ADDRESS:./server ./server_backup/ 
+rsync -arvz --delete -e "ssh -i $SSHKEY" $USER@$SERVER_ADDRESS:./server/* ./server_backup/
 echo "JOB DONE"
