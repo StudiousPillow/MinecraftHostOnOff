5e252304d6fdc0de23227bb67723972dc3ce802a
StudiousPillow <151650879+StudiousPillow@users.noreply.github.com>
2025-03-02 22:23:51 +0100

    backup typo

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 source params.sh
 mkdir server_backup
-rsync -arvz --files-from=files_to_backup.txt -e "ssh -i $SSHKEY" $USER@$SERVER_ADDRESS:~/ ./server_backup/ 
+rsync -arvz --files-from=files_to_backup.txt --delete -e "ssh -i $SSHKEY" $USER@$SERVER_ADDRESS:. ./server_backup/ 
 echo "JOB DONE"
